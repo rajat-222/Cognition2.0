0acbe5c4b1fb416ae4282dc39bf2073ee50c8083
Priyanshu <priyanshubutola@gmail.com>
2023-07-11 22:03:43 +0530

    added code for backend ses 2

---
+++ Backend/Code/Ses 2/users.js
@@ -0,0 +1,22 @@
+module.exports.sayHi = (name) => {
+  console.log(`Hi ${name}`);
+};
+
+module.exports.name = "Priyanshu";
+
+// const secret = "shdjhsdj";
+
+// const name = "my name";
+// const age = 19;
+// const pwd = "skdjsj";
+
+// const address = "asdjaskj";
+
+// user = {
+//   Name: "Rohit",
+//   SAP: "500105010",
+// };
+
+// module.exports = { name, age, user };
+
+// console.log(module);
